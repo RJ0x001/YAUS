c9f6fb1a347d689713e838f9ead8c4da2a17c86b
RJ0x001 <artem.e.pavlenko@gmail.com>
2018-07-31 23:20:51 +0300

    change short_url generation

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
-import string,random
 from urlparse import urlparse
 import sqlite3
-
-
-def url_gen():
-    data = string.ascii_lowercase + string.digits
-    short_url = ''.join(random.choice(data) for key in range(5))
-    return short_url
+import base64
 
 app = Flask(__name__)
 
@@ -16,16 +10,21 @@ app = Flask(__name__)
 
 def home():
     if request.method == 'POST':
-        conn = sqlite3.connect('first_database.db')
+        conn = sqlite3.connect('main.db')
         cursor = conn.cursor()
         user_url = str(request.form.get('url'))
         if urlparse(user_url).scheme == '':
             user_url = 'http://' + user_url
-        short_url = url_gen()
+        cursor.execute("CREATE TABLE IF NOT EXISTS `YAUS` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT, `URL` TEXT NOT NULL, `SHORT_URL` TEXT)")
         cursor.execute("SELECT ID FROM YAUS WHERE URL =?", [user_url])
         data_ex = cursor.fetchone()
         if data_ex == None :
-            cursor.execute("INSERT INTO YAUS (URL, SHORT_URL) VALUES ('%s', '%s')" % (user_url, short_url))
+            cursor.execute("INSERT INTO YAUS (URL) VALUES ('%s')" % (user_url))
+            cursor.execute("SELECT ID FROM YAUS WHERE URL =?", [user_url])
+            current_id = cursor.fetchone()
+            id_work= str(current_id[0]).encode('ascii')
+            short_url= str(base64.b64encode(id_work)).rstrip("=")
+            cursor.execute("UPDATE YAUS SET (SHORT_URL) = ('%s') WHERE ID = ('%s')" % (short_url, current_id[0]))
         else:
             cursor.execute("SELECT SHORT_URL FROM YAUS WHERE ID =?", [data_ex[0]])
             short_url = cursor.fetchone()
@@ -43,7 +42,7 @@ def shortened():
 
 @app.route('/<short_url>/')
 def redirect_url(short_url):
-    conn = sqlite3.connect('first_database.db')
+    conn = sqlite3.connect('main.db')
     cursor = conn.cursor()
     cursor.execute("SELECT URL FROM YAUS WHERE SHORT_URL =?", [short_url])
     short_url_link = cursor.fetchone()
