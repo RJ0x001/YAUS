1777a8d62b065205cd32f22cd30933cb63cf5e07
RJ0x001 <artem.e.pavlenko@gmail.com>
2018-08-19 22:19:22 +0300

    add comments

--- config_yaus.py
+++ config_yaus.py
@@ -8,17 +8,16 @@ from sqlalchemy.ext.declarative import declarative_base
 import base64
 
 
-app = Flask(__name__)
-engine = create_engine('sqlite:///main.db', connect_args={'check_same_thread': False})
+app = Flask(__name__)   # create an instance of the Flask class
+engine = create_engine('sqlite:///main.db', connect_args={'check_same_thread': False})  # create db engine
 Session = sessionmaker(bind=engine)
 Session.configure(bind=engine)
-session = Session()
-test_url = 'rambler.ru'
-Base = declarative_base()
+session = Session()  # make session
+Base = declarative_base()   # class Base
 
 
-class YAUS_t(Base):
-    __tablename__ = 'yaus_t'
+class YAUS_t(Base):    # make db
+    __tablename__ = 'yaus_t'   # name of table
     id = Column(Integer, primary_key=True, autoincrement=True)
     url = Column(String)
     short_url = Column(String)
@@ -28,4 +27,4 @@ class YAUS_t(Base):
         self.short_url = short_url
 
 
-Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)    # create!
--- main.py
+++ main.py
@@ -3,42 +3,58 @@ from config_yaus import *
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    '''
+    Main page. Get URL from user, validate it for include http part and compare with existing URLs in database.
+    If URL already in database return existing short link, else generate new and add new row in database.
+    :return: If users's URL not empty -- redirect to new page with shorten URL.
+    '''
     if request.method == 'POST':
         user_url = str(request.form.get('url'))
-        if urlparse(user_url).scheme == '':
-            user_url = 'http://' + user_url
-        ex_id = session.query(YAUS_t.id).filter_by(url=user_url).scalar()
-        if ex_id is None:
-            try:
-                last_id = session.query(YAUS_t.id).order_by(YAUS_t.id.desc()).first()
-                real_id = last_id[0] + 1
-            except TypeError:
-                real_id = 1
-            id_work = str(real_id).encode('ascii')
-            short_url = str(base64.b64encode(id_work)).rstrip("=")
-            session.add(YAUS_t(user_url, short_url))
+        if user_url != '':  # check for empty string
+            if urlparse(user_url).scheme == '':   # check http part
+                user_url = 'http://' + user_url
+            ex_id = session.query(YAUS_t.id).filter_by(url=user_url).scalar()   # is the same URL already exist in db?
+            if ex_id is None:  # if URL is new
+                try:
+                    last_id = session.query(YAUS_t.id).order_by(YAUS_t.id.desc()).first() # get last row id
+                    real_id = last_id[0] + 1     # id of current new URL
+                except TypeError:
+                    real_id = 1   # table is empty, this is the first URL in db
+                id_work = str(real_id).encode('ascii')  # start encode id, first to ascii
+                short_url = str(base64.b64encode(id_work)).rstrip("=")  # encode id with base64 algoritm, delete last '=' char
+                session.add(YAUS_t(user_url, short_url))    # add both URLs in db
+            else:
+                short_url = session.query(YAUS_t.short_url).filter_by(id=ex_id).scalar() # URL already exist in db, get short URL
+            session.commit()
+            short_url_link = '127.0.0.1:5000/%s' % short_url    # make link to redirect
+            return redirect(url_for('shortened', url=short_url_link))   # redirect to shortened page
         else:
-            short_url = session.query(YAUS_t.short_url).filter_by(id=ex_id).scalar()
-        session.commit()
-        short_url_link = 'localhost:5000/%s' % short_url
-        return redirect(url_for('shortened', url=short_url_link))
+            return render_template('home.html')  # empty user's URL
     else:
-        return render_template('home.html')
+        return render_template('home.html')  # wrong request method
 
 
 @app.route('/shortened/')
 def shortened():
+    '''
+    Page with shorten URL.
+    :return: Shorten URL with link to users page
+    '''
     url = request.args.get('url')
-    return render_template('shortened.html', url=url)
+    return render_template('shortened.html', url=url)  # add short url to html page
 
 
 @app.route('/<short_url>/')
 def redirect_url(short_url):
-    short_url_link = session.query(YAUS_t.url).filter_by(short_url=short_url).scalar()
+    '''
+    :param short_url: generated short URL
+    :return: redirected to user page
+    '''
+    short_url_link = session.query(YAUS_t.url).filter_by(short_url=short_url).scalar() # get user's URL from db with short one
     if short_url_link is not None:
-        return redirect(short_url_link)
-    return redirect(url_for('home'))
+        return redirect(short_url_link)  # redirect to url
+    return redirect(url_for('home'))    # redirect to home page if short_url doesn't exist in db
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()   # run the program
